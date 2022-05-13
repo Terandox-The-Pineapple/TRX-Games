c457c609f75d7a84d0239ae6203110a15570b88b
Terandox <rene-gilbert99@web.de>
2022-05-13 09:49:02 +0200

    Pong menu testing
    Added stuff to pong and added stuff to test if menus work properly

--- pong.lua
+++ pong.lua
@@ -6,9 +6,9 @@ if fs.exists("class") == false then shell.run("wget https://raw.githubuserconten
 local class_lib = require("class")
 
 -- Variables
-local exit = false
 local pause = false
-local set_menu = false
+local buffer = false
+local exit = false
 -- Classes
 local points = { posX = 1, posY = 1, value = 0, color = colours.white }
 function points:draw()
@@ -19,12 +19,84 @@ end
 
 function points:undraw()
     term.setCursorPos(self.posX, self.posY)
+    term.setBackgroundColor(game_utils.backgrounds.backgroundlist[game_utils.backgrounds.selected].render[self.posX][self.posY].color)
+    term.write(" ")
 end
 
+function points:add()
+    self.value = self.value + 1
+    if self.value >= 9 then
+        self.color = colours.red
+    end
+end
+
+function points:reset()
+    self.value = 0
+    self.color = colours.white
+end
+
+points = class_lib.class:new(points)
+-- Points
+local player_points = points:new({ posX = 24, posY = 3 })
+local enemy_points = points:new({ posX = 28, posY = 3 })
 -- Entitys
-local player = game_utils.add_player("player_1")
-local enemy = game_utils.add_enemy("enemy_1")
-local ball = game_utils.add_other("ball_1")
+local player = game_utils.add_player(2, 7, "player_1")
+function player:reset()
+    self.posX = 2
+    self.posY = 7
+end
+
+local enemy = game_utils.add_enemy(50, 7, "enemy_1")
+function enemy:reset()
+    self.posX = 50
+    self.posY = 7
+end
+
+local ball = game_utils.add_other(48, 9, "ball_1")
+function ball:reset()
+    self.posX = 48
+    self.posY = 9
+    self.motionX = -1
+    self.motionY = math.random(2)
+    if self.motionY == 2 then
+        self.motionY = -1
+    end
+end
+
+function ball:bounce()
+    self.motionY = self.motionY * -1
+end
+
+function ball:bounce_back()
+    self.motionX = self.motionX * -1
+end
+
+function ball:goal()
+    if self.posX == 1 then
+        if enemy_points.value >= 9 then
+            return end_game(false)
+        else
+            enemy_points:undraw()
+            enemy_points:add()
+            enemy_points:draw()
+            os.sleep(0.5)
+            self:reset()
+            return false
+        end
+    elseif self.posX == 51 then
+        if player_points.value >= 9 then
+            return end_game(true)
+        else
+            player_points:undraw()
+            player_points:add()
+            player_points:draw()
+            os.sleep(0.5)
+            self:reset()
+            return false
+        end
+    end
+end
+
 -- menus
 local main_menu = game_utils.add_menu("main")
 local end_menu = game_utils.add_menu("end")
@@ -72,19 +144,81 @@ game_utils.add_controller_key(end_menu_controll, "Select", keys.enter, function(
     end
 end)
 game_utils.add_controller_key(game_controll, "Up", keys.up, function()
-    if pause == false then player:moveUp() end
+    if pause == false and player.posY > 2 then buffer = "U" end
 end)
 game_utils.add_controller_key(game_controll, "Down", keys.down, function()
-    if pause == false then player:moveDown() end
+    local sizeX, sizeY = player:getSize()
+    if pause == false and player.posY < (19 - sizeY) then buffer = "D" end
 end)
 game_utils.add_controller_key(game_controll, "Pause", keys.space, function()
     if pause then
         pause = false
     elseif pause == false then
         pause = true
+        buffer = false
     end
 end)
+-- Render Inits
+for x = 1, 51, 1 do
+    for y = 1, 19, 1 do
+        if x == 1 or x == 51 or y == 1 or y == 19 or (x >= 20 and x <= 26) or (y >= 8 and y <= 12) then
+            menu_back.render[x][y].color = colours.white
+        else
+            menu_back.render[x][y].color = colours.black
+        end
+    end
+end
+for x = 1, 51, 1 do
+    for y = 1, 19, 1 do
+        if y == 1 or y == 19 or x == 26 then
+            game_back.render[x][y].color = colours.white
+        else
+            game_back.render[x][y].color = colours.black
+        end
+    end
+end
+player.render[1][1].color = colours.lightGrey
+player.render[1][2].color = colours.lightGrey
+player.render[1][3].color = colours.lightGrey
+player.render[1][4].color = colours.lightGrey
+enemy.render[1][1].color = colours.lightGrey
+enemy.render[1][2].color = colours.lightGrey
+enemy.render[1][3].color = colours.lightGrey
+enemy.render[1][4].color = colours.lightGrey
+ball.render[1][1].color = colours.grey
 -- Game Functions
 function start_game()
+    print("game started")
+end
+
+function end_game(win)
 
 end
+
+function open_menu(main)
+    game_utils.change_background(menu_back)
+    if main then
+        game_utils.change_menu(main_menu, 22, 9)
+        repeat
+            main_menu_controll:start(function()
+                while true do
+                    --nothing
+                end
+            end)
+        until main_menu_controll.finished
+        return exit
+    else
+        game_utils.change_menu(end_menu, 22, 9)
+        repeat
+            end_menu_controll:start(function()
+                while true do
+                    --nothing
+                end
+            end)
+        until end_menu_controll.finished
+        return exit
+    end
+end
+
+local para = open_menu(true)
+print(para and 'true' or 'false')
